594dd9f53f584d9034b8ccc7805a9c000188adfa
ninjatuna3527 <noreply@local>
2026-06-05 13:51:40 +0100

    wsgi: use SSL_CERTFILE/SSL_KEYFILE when run directly

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,14 @@ This module exposes the Flask `app` as `app` so WSGI servers such as
 """
 
 from src.app import app
+import os
 
 
 if __name__ == '__main__':
     # allow running locally for quick debugging (not for production)
-    app.run(host='0.0.0.0', port=5000)
+    ssl_cert = os.getenv('SSL_CERTFILE')
+    ssl_key = os.getenv('SSL_KEYFILE')
+    if ssl_cert and ssl_key:
+        app.run(host='0.0.0.0', port=5000, ssl_context=(ssl_cert, ssl_key))
+    else:
+        app.run(host='0.0.0.0', port=5000)
